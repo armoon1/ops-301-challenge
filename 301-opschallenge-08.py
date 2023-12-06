d520ce48423cbbd893e494159e1cdb2d010ae6f5
armoon1 <armoon@yahoo.com>
2023-12-06 15:12:16 -0800

    test

---
+++ 301-opschallenge-08.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      12/06/2023
+# Purpose:                      Ops 08
+
+#Assign to a variable a list of ten string elements.
+fruits = ['banana', 'apple', 'kiwi', 'cherry','melon', 'grapes','mango', 'coconut', 'strawberry' ,'blackberry' ]
+#Print the fourth element of the list.
+print(fruits)
+print(fruits[3])
+#Print the sixth through tenth element of the list.
+print(fruits[5:10])
+#Change the value of the seventh element to “onion”.
+fruits[6]='onion'
+print(fruits)
+
