02f632edf7781bfe7eefe7e39f9edd4c09a42352
armoon1 <armoon@yahoo.com>
2023-12-07 22:48:43 -0800

    Lab09

---
+++ 302-opschallenge-09.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      12/07/2023
+# Purpose:                      Ops 09
+#Create if statements using these logical conditionals below. 
+#Each statement should print information to the screen depending on if the condition is met.
+# Variables
+a = 5
+b = 10
+# Equals: a == b
+if a == b:
+    print("a is equal to b")
+# Not Equals: a != b
+#elif a != b:
+#    print("a is not equal to b")
+# Less than: a < b
+#elif a < b:
+#    print("a is less than b")
+# Less than or equal to: a <= b
+#elif a <= b:
+#    print("a is less than or equal to b")
+# Greater than: a > b
+#elif a > b:
+#    print("a is greater than b")
+# Greater than or equal to: a >= b
+#elif a >= b:
+#    print("a is greater than or equal to b")
+
+# If none of the above conditions are met
+else:
+    print("None of the conditions are met")
