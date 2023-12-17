fc771195960f29e5ea7011f8299e3150990ffce3
armoon1 <armoon@yahoo.com>
2023-12-17 15:10:22 -0800

    Directory creation

--- 301-opschallenge-07.py
+++ 301-opschallenge-07.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 # Author:                       SEYED MEHDI HASHEMI SOHI
 # Date of latest revision:      12/05/2023
-# Purpose:                      Import libraries
+# Purpose:                      Directory Creation
  
 
+# Import libraries
 import os
 
-# Declaration of variables
-fruits = ['apple','banana', 'kiwi']
-### Read user input here into a variable
-user_input=input("Enter name: ")
-print("You entered: ", user_input)
 # Declaration of functions
-
-### Declare a function here
-
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
+def generate_directory_structure(user_path):
+    for (root, dirs, files) in os.walk(user_path):
+        # Add a print command here to print ==root==
+        print(f"Root: {root}")
+        # Add a print command here to print ==dirs==
+        print(f"Directories: {dirs}")
+        # Add a print command here to print ==files==
+        print(f"Files: {files}")
 
 # Main
+if __name__ == "__main__":
+    # Read user input here into a variable
+    user_path = input("Enter the directory path: ")
 
-### Pass the variable into the function here
+    # Pass the variable into the function here
+    generate_directory_structure(user_path)
 
-# End
