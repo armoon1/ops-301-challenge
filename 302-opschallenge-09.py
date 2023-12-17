da96712844719bb2a3f8b3863d9bbdbef243e892
armoon1 <armoon@yahoo.com>
2023-12-17 15:24:10 -0800

    Conditional

--- 302-opschallenge-09.py
+++ 302-opschallenge-09.py
@@ -4,28 +4,31 @@
 # Purpose:                      Ops 09
 #Create if statements using these logical conditionals below. 
 #Each statement should print information to the screen depending on if the condition is met.
-# Variables
-a = 5
-b = 10
-# Equals: a == b
+
+# Sample values for a and b
+a = 10
+b = 5
+
+# If statement using equality
 if a == b:
     print("a is equal to b")
-# Not Equals: a != b
-#elif a != b:
-#    print("a is not equal to b")
-# Less than: a < b
-#elif a < b:
-#    print("a is less than b")
-# Less than or equal to: a <= b
-#elif a <= b:
-#    print("a is less than or equal to b")
-# Greater than: a > b
-#elif a > b:
-#    print("a is greater than b")
-# Greater than or equal to: a >= b
-#elif a >= b:
-#    print("a is greater than or equal to b")
-
-# If none of the above conditions are met
+elif a != b:
+    print("a is not equal to b")
 else:
-    print("None of the conditions are met")
+    print("This should not happen, there's an issue with the conditions")
+
+# If statement using less than
+if a < b:
+    print("a is less than b")
+elif a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("This should not happen, there's an issue with the conditions")
+
+# If statement using greater than
+if a > b:
+    print("a is greater than b")
+elif a <= b:
+    print("a is less than or equal to b")
+else:
+    print("This should not happen, there's an issue with the conditions")
